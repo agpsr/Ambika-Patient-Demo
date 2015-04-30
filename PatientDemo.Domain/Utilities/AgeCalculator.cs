bbd8fac504f68fbf2ef97c22028cd147b74b37d5
Ambika <ambikapsr@gmail.com>
2015-04-30 13:30:42 +0100

    Update AgeCalculator.cs

--- PatientDemo.Domain/Utilities/AgeCalculator.cs
+++ PatientDemo.Domain/Utilities/AgeCalculator.cs
@@ -7,21 +7,26 @@ namespace PatientDemo.Domain.Utilities
 		public static string Calculate(DateTime Tdate, DateTime DOB)
 		{
 			int year = 0;
-			int month;
+			int month = 0;
 
 			if (Tdate.Month < DOB.Month)
 			{
 				int y1 = Tdate.Year - 1;
 				year = y1 - DOB.Year;
+				int m1 = 12 - DOB.Month;
+				month = m1 + Tdate.Month;
 			}
 
-			if ((Tdate.Month == DOB.Month) || (DOB.Month < Tdate.Month))
+			if (DOB.Month < Tdate.Month)
+			{
+				year = Tdate.Year - DOB.Year;
+				month = Tdate.Month - DOB.Month;
+			}
+
+			if (Tdate.Month == DOB.Month)
 			{
 				year = Tdate.Year - DOB.Year;
 			}
-
-			int m1 = 12 - DOB.Month;
-			month = m1 + Tdate.Month;
 
 			return string.Format("Age : {0} years {1} months", year, month);
 		}
